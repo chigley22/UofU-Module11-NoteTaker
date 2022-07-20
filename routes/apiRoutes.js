a5918ae90e3958e6644ef524fd10b014f8dbe1ea
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-07-19 22:31:56 -0600

    deploy push

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,16 +5,14 @@ const readFileAsync = util.promisify(fs.readFile);
 
 var noteContents;
 module.exports = function (app) {
-  // Populate the saved notes from the JSON file
+
   app.get("/api/notes", function (req, res) {
-    readFileAsync("db/db.json", "utf8").then(function (data) {
+    readFileAsync("/Develop/db/db.json", "utf8").then(function (data) {
       noteContents = JSON.parse(data);
       res.json(noteContents);
     });
   });
 
-  // Posts new notes to the JSON file when entered and saved
-  // Had trouble setting the new Id number - this seems to work correctly on heroku -but not on my localhost
   app.post("/api/notes", function (req, res) {
     let newNote = req.body;
     let lastId = 0;
@@ -24,7 +22,7 @@ module.exports = function (app) {
     let newId = lastId + 1;
     newNote["id"] = newId;
     noteContents.push(newNote);
-    writeFileAsync("db/db.json", JSON.stringify(noteContents)).then(
+    writeFileAsync("/Develop/db/db.json", JSON.stringify(noteContents)).then(
       function () {
         console.log("Note has been updated");
       }
@@ -32,14 +30,13 @@ module.exports = function (app) {
     res.json(newNote);
   });
 
-  // Function to delete notes when trash can is clicked
   app.delete("/api/notes/:id", function (req, res) {
     let chosenId = parseInt(req.params.id);
     for (let i = 0; i < noteContents.length; i++) {
       if (chosenId === noteContents[i].id) {
         noteContents.splice(i, 1);
         let noteJSON = JSON.stringify(noteContents, null, 2);
-        writeFileAsync("db/db.json", noteJSON).then(function () {
+        writeFileAsync("/Develop/db/db.json", noteJSON).then(function () {
           console.log("Note has been deleted");
         });
       }
