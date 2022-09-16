7b45aac91ead8d0215723df2ed8fe1a8c1d7360f
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-09-16 03:12:37 -0600

    reversing route path back to original as that did not correct heroku issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
